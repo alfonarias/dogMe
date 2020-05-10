d39e272f31ab670914626ea6c5f8295108dbd4ad
Alvaro Foguet <afoguetc@everis.com>
2020-05-10 14:27:51 +0200

    se anade calendario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,11 +6,14 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 
+import { NgCalendarModule  } from 'ionic2-calendar';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgCalendarModule,
     RouterModule.forChild([
       {
         path: '',
@@ -18,6 +21,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  entryComponents: [HomePage]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID} from '@angular/core';
+import * as moment from 'moment';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  event = {
+    title: '',
+    desc: '',
+    startTime: '',
+    endTime:'',
+    allDay: false
+  }
 
-}
+  minDate = new Date().toISOString();
+
+  eventSource = [];
+  viewTitle: string;
+  selectedDay = new Date();
+
+  calendar = {
+    mode: 'month',
+    currentDate: this.selectedDay,
+    startingDayMonth : 1,
+    startingDayWeek : 1,
+    formatDayHeader : 'EEEEE'
+  };
+
+  @ViewChild(CalendarComponent, {static: false}) myCal: CalendarComponent;
+
+  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID)private locale: string) {}
+
+  ngOnInit(){
+    this.resetEvent();
+  }
+
+  resetEvent(){
+    this.event = {
+      title: '',
+      desc: '',
+      startTime: new Date().toISOString(),
+      endTime: new Date().toISOString(),
+      allDay: false
+    }
+  }
+
+  addEvent(){
+    let eventCopy = {
+      title: this.event.title,
+      desc: this.event.desc,
+      startTime: new Date(this.event.startTime),
+      endTime: new Date(this.event.endTime),
+      allDay: this.event.allDay
+    }
+    
+    if (eventCopy.allDay) {
+      let start = eventCopy.startTime;
+      let end = eventCopy.endTime;
+
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
+      eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
+    }
+
+    this.eventSource.push(eventCopy);
+    this.myCal.loadEvents();
+    this.resetEvent();
+
+  }
+
+  changeMode(mode) {
+    this.calendar.mode = mode;
+  }
+
+  back() {
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slidePrev();
+  }
+  
+  next() {
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slideNext();
+  }
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  onTimeSelected(ev) {
+    this.selectedDay = ev.selectedTime;
+  }
+
+  async onEventSelected(event) {
+    let start = formatDate(event.startTime, 'medium', this.locale);
+    let end = formatDate(event.endTime, 'medium', this.locale);
+
+    const alert = await this.alertCtrl.create({
+      header: event.title,
+      subHeader: event.desc,
+      message: 'Desde: ' + start + '<br><br>Hasta: ' + end,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  }
