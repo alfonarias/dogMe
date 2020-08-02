dbc897d87b6449f49f5f9c63f80aa809682a5144
Alfonso Arias <alfonarias93@gmail.com>
2020-08-02 15:20:57 +0100

    eliminar comentarios

--- src/app/dogs/home/home.page.ts
+++ src/app/dogs/home/home.page.ts
@@ -20,36 +20,5 @@ export class HomePage implements OnInit {
   private dogsSub: Subscription;
   private selectedDogSub: Subscription;
 
-  // ngOnInit() {}
-
-  ngOnInit() {
-    // this.dogsSub = this.dogsService.dogs.subscribe(dogs => {
-    //   this.dogs = dogs;
-    // });
-    // this.selectedDogSub = this.dogsService.selectedDog.subscribe(
-    //   dog => (this.selectedDog = dog)
-    // );
-    // console.log('on init dogs', this.dogs);
-    // console.log('on init selected', this.selectedDog);
-  }
-
-  // ionViewWillEnter() {
-  //   this.dogsService.fetchDogs().subscribe();
-
-  //   console.log('on willEnter dogs', this.dogs);
-  //   console.log('on willEnter selected', this.selectedDog);
-  // }
-
-  // createPopOverForDogSelector() {
-  //   this.popOverCtrl
-  //     .create({ component: DogSelectorPage, showBackdrop: false })
-  //     .then(popoverElm => {
-  //       popoverElm.present();
-  //       // popoverElm.onDidDismiss();
-  //     });
-  // }
-
-  // showDog(){
-  //   this.selectedDog.
-  // }
+  ngOnInit() {}
 }
