3d3cdf82e0f084a86053224964d558e2ad555222
Alfonso Arias <alfonarias93@gmail.com>
2021-03-08 22:00:54 +0000

    seleccionar fecha

--- src/app/core/services/date-utils.service.ts
+++ src/app/core/services/date-utils.service.ts
@@ -1,9 +1,99 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+// class Week {
+//   constructor(public start: number, public end: number) {}
+// }
 export class DateUtilsService {
+  constructor() {}
 
-  constructor() { }
+  public minYear = 1990;
+  public maxYear = new Date().getFullYear() + 20;
+  //public startingYear = new Date().getFullYear();
+
+  monthsNumberAndName = [
+    { monthNumber: 0, monthName: 'Enero' },
+    { monthNumber: 1, monthName: 'Febrero' },
+    { monthNumber: 2, monthName: 'Marzo' },
+    { monthNumber: 3, monthName: 'Abril' },
+    { monthNumber: 4, monthName: 'Mayo' },
+    { monthNumber: 5, monthName: 'Junio' },
+    { monthNumber: 6, monthName: 'Julio' },
+    { monthNumber: 7, monthName: 'Agosto' },
+    { monthNumber: 8, monthName: 'Septiembre' },
+    { monthNumber: 9, monthName: 'Octubre' },
+    { monthNumber: 10, monthName: 'Noviembre' },
+    { monthNumber: 11, monthName: 'Diciembre' },
+  ];
+
+  days = ['D', 'L', 'M', 'X', 'J', 'V', 'S']; //date.getDay() usa domingo el primero
+
+  months = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
+
+  getWeeksInMonth(year: number, month: number) {
+    // const weeks: Week[] = [];
+    const weeks = [];
+    const firstDay: Date = new Date(year, month, 1);
+    const lastDay: Date = new Date(year, month + 1, 0);
+    const daysInMonth: number = lastDay.getDate();
+    let dayOfWeek: number = firstDay.getDay();
+    let start: number;
+    let end: number;
+
+    for (let i = 1; i < daysInMonth + 1; i++) {
+      if (dayOfWeek === 0 || i === 1) {
+        start = i;
+      }
+
+      if (dayOfWeek === 6 || i === daysInMonth) {
+        end = i;
+
+        if (start !== end) {
+          // weeks.push(new Week(start, end));
+          weeks.push({ Start: start, End: end });
+        }
+      }
+
+      dayOfWeek = new Date(year, month, i).getDay();
+    }
+
+    return weeks;
+  }
+
+  getMonthNumber(month: string) {
+    const monthNumber = this.monthsNumberAndName
+      .filter(m => m.monthName === month)
+      .map(m => m.monthNumber)[0];
+    if (typeof monthNumber === 'undefined') {
+      throw new Error('Month not found');
+    }
+    return monthNumber;
+  }
+
+  getDaysArray(year: number, month: number) {
+    let monthIndex = month; // 0-Jan to 11-Dec
+    let names = this.days;
+    let date = new Date(year, monthIndex, 1);
+    let result = [];
+    while (date.getMonth() === monthIndex) {
+      result.push({ dayNumber: date.getDate(), dayName: names[date.getDay()] });
+      date.setDate(date.getDate() + 1);
+    }
+    return result;
+  }
 }
--- src/app/dogs/home/create-vac/edit-vac/edit-vac.page.ts
+++ src/app/dogs/home/create-vac/edit-vac/edit-vac.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateUtilsService } from 'src/app/core/services/date-utils.service';
 
 @Component({
   selector: 'app-edit-vac',
@@ -6,10 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-vac.page.scss'],
 })
 export class EditVacPage implements OnInit {
+  constructor(private datesUtilsService: DateUtilsService) {}
 
-  constructor() { }
+  maxYear = this.datesUtilsService.maxYear;
+  minYear = this.datesUtilsService.minYear;
+  todayYear = new Date().getFullYear();
+  todayMonth = new Date().getMonth();
+
+  months = this.datesUtilsService.months;
+
+  monthDays = [];
+  monthNumberDays = [];
+  monthNameDays = [];
+
+  selectedYear = this.todayYear;
+  selectedMonth = this.todayMonth;
+  selectedDay = 1; //inicial con alguno
+
+  monthHasBeenSelected = false;
+  dayHasBeenSelected = false;
+  dateHasBeenSelected = this.monthHasBeenSelected && this.dayHasBeenSelected;
 
   ngOnInit() {
+    this.updateMonthDaysList(
+      this.datesUtilsService.getDaysArray(this.todayYear, this.todayMonth)
+    );
   }
 
+  onMonthChange(event) {
+    let month = event.detail.value;
+    let monthNumber = this.datesUtilsService.getMonthNumber(month);
+    let monthDaysList = this.datesUtilsService.getDaysArray(
+      this.selectedYear,
+      monthNumber
+    );
+
+    this.monthHasBeenSelected = true;
+    this.selectedMonth = monthNumber;
+    this.updateMonthDaysList(monthDaysList);
+    this.updateBoolean();
+  }
+
+  onDaySelected(event) {
+    let day = event.detail.value;
+    this.dayHasBeenSelected = true;
+    this.updateBoolean();
+  }
+
+  onYearChange(event) {
+    let year = new Date(event.detail.value).getFullYear();
+    this.selectedYear = year;
+
+    this.updateMonthDaysList(
+      this.datesUtilsService.getDaysArray(year, this.selectedMonth)
+    );
+  }
+
+  updateMonthDaysList(daysArray: any[]) {
+    this.monthDays = daysArray;
+    this.monthNumberDays = daysArray.map(m => m.dayNumber);
+    this.monthNameDays = daysArray.map(m => m.dayName);
+  }
+
+  updateBoolean() {
+    this.dateHasBeenSelected =
+      this.monthHasBeenSelected && this.dayHasBeenSelected;
+  }
 }
